df27bfbb5cb21e8bf92794ec4a0ddd97d00486f6
Kübra <54949171+kubraturkoglu@users.noreply.github.com>
2021-08-19 13:21:02 +0300

    Add files via upload

---
+++ rotate_image/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(41, 369);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Minimum = -360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(253, 69);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(454, 369);
+            this.trackBar2.Maximum = 360;
+            this.trackBar2.Minimum = -360;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(248, 69);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(64, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Angle ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(472, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Compass";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ rotate_image/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int en1 = 150, boy1 = 150, koordinatX1 = 150, koordinatY1 = 150;
+        int en2 = 170, boy2 = 170, koordinatX2 = 400, koordinatY2 = 150;
+
+      
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            angle1 = trackBar2.Value;
+            Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            angle = trackBar1.Value;
+            Invalidate();
+        }
+
+        float angle, angle1;
+        Bitmap image = Properties.Resources.airground250;
+        Bitmap image1 = Properties.Resources.pusula;
+        
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Matrix matrix = new Matrix();
+
+
+            
+            Graphics z = e.Graphics;
+           
+
+            g.RotateTransform(angle, MatrixOrder.Append);
+            g.TranslateTransform(en1, boy1, MatrixOrder.Append);
+            g.DrawImage(image, -koordinatX1 / 2, -koordinatY1 / 2, koordinatX1, koordinatY1);
+
+
+            e.Graphics.ResetTransform();
+
+            z.RotateTransform(angle1, MatrixOrder.Append);
+            z.TranslateTransform(koordinatX2, koordinatY2, MatrixOrder.Append);
+            z.DrawImage(image1, -en2 / 2, -boy2 / 2, en2, boy2);
+            /*
+
+            if (angle != 0)
+            {
+               
+                g.TranslateTransform(cx, cy, MatrixOrder.Append);
+                g.DrawImage(image, -rx / 2, -ry / 2, rx, ry);
+            }
+            else if (angle1 != 0)
+            {
+               
+                z.TranslateTransform(cx, cy, MatrixOrder.Append);
+                z.DrawImage(image1, -rx / 2, -ry / 2, rx, ry);
+            }
+
+            */
+
+
+        }
+    }
+}
